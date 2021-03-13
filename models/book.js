c7b0773365e7aff36bb7d5db7b03df74f73f0722
Nahidul <nahisl12@gmail.com>
2021-03-13 15:33:45 +0000

    Fileupload database

--- models/book.js
+++ models/book.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
-const path = require("path");
+// const path = require("path");
 
-const coverImageBasePath = "uploads/bookCovers";
+// const coverImageBasePath = "uploads/bookCovers";
 
 const bookSchema = new mongoose.Schema({
   title: {
@@ -24,7 +24,11 @@ const bookSchema = new mongoose.Schema({
     required: true,
     default: Date.now,
   },
-  coverImageName: {
+  coverImage: {
+    type: Buffer,
+    required: true,
+  },
+  coverImageType: {
     type: String,
     required: true,
   },
@@ -36,10 +40,50 @@ const bookSchema = new mongoose.Schema({
 });
 
 bookSchema.virtual("coverImagePath").get(function () {
-  if (this.coverImageName != null) {
-    return path.join("/", coverImageBasePath, this.coverImageName);
+  if (this.coverImage != null && this.coverImageType != null) {
+    return `data:${
+      this.coverImageType
+    };charset=utf-8;base64,${this.coverImage.toString("base64")}`;
   }
 });
 
+// bookSchema.virtual("coverImagePath").get(function () {
+//   if (this.coverImageName != null) {
+//     return path.join("/", coverImageBasePath, this.coverImageName);
+//   }
+// });
+
+// const bookSchema = new mongoose.Schema({
+//   title: {
+//     type: String,
+//     required: true,
+//   },
+//   description: {
+//     type: String,
+//   },
+//   publishDate: {
+//     type: Date,
+//     required: true,
+//   },
+//   pageCount: {
+//     type: Number,
+//     required: true,
+//   },
+//   createdAt: {
+//     type: Date,
+//     required: true,
+//     default: Date.now,
+//   },
+//   coverImageName: {
+//     type: String,
+//     required: true,
+//   },
+//   author: {
+//     type: mongoose.Schema.Types.ObjectId,
+//     required: true,
+//     ref: "Author",
+//   },
+// });
+
 module.exports = mongoose.model("Book", bookSchema);
-module.exports.coverImageBasePath = coverImageBasePath;
+// module.exports.coverImageBasePath = coverImageBasePath;
---
+++ public/javascripts/fileUpload.js
@@ -0,0 +1,13 @@
+FilePond.registerPlugin(
+  FilePondPluginImagePreview,
+  FilePondPluginImageResize,
+  FilePondPluginImageFileEncode
+);
+
+FilePond.setOptions({
+  stylePanelAspectRatio: 150 / 100,
+  imageResizeTargetWidth: 100,
+  imageResizeTargetHeight: 150,
+});
+
+FilePond.parse(document.body);
